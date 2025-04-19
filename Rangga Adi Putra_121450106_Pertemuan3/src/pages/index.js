5767e343d77c26569da362b942dcd8287209d82c
RanggaAdp <rangga.121450106@student.itera.ac.id>
2025-04-19 14:43:13 +0700

    Add files via upload

---
+++ Rangga Adi Putra_121450106_Pertemuan3/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Statistik Buku</h1>
+      {/* Statistik buku akan ditampilkan di sini */}
+      <p>Fitur statistik akan ditambahkan...</p>
+    </div>
+  );
+};
+
+export default Stats;
